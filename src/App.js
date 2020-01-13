95f99077b366e93e5283f3529941fe3aaf97641c
Raina Gupta <rainagupta@Rainas-MBP.home>
2020-01-12 22:57:21 -0500

    shape constructors

--- src/App.js
+++ src/App.js
@@ -5,6 +5,120 @@ import p5 from "p5";
 import logo from './logo.svg';
 import './App.css';
 import Sketch from 'react-p5';
+import p5 from "p5";
+
+
+class Shape {
+
+  positionX;
+  positionY;
+
+  constructor(p, positionX, positionY) {
+    this.p = p;
+    this.positionX = positionX;
+    this.positionY = positionY;
+  }
+
+  moveUp(distance){
+    this.positionY = this.positionY + distance;
+  }
+
+
+  moveDown(distance){
+    this.positionY = this.positionY - distance;
+  }
+
+
+  moveLeft(distance) {
+    this.positionX = this.positionX - distance;
+   }
+
+  moveRight(distance ) {
+     this.positionX = this.positionX + distance;
+   }
+
+}
+
+
+/*CIRCLE CLASS*/
+
+class Circling extends Shape{
+
+  radius;
+  shapeType;
+  
+  constructor(positionX, positionY, radius) {
+    super();
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.shapeType = "circle";
+    this.radius = radius;
+  }
+
+
+
+ }
+
+
+ /*SQUARE CLASS*/
+
+ class Square extends Shape{
+
+  shapeType;
+  sideSize;
+
+  constructor(positionx, positionY, sideSize) {
+
+    super();
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.shapeType = "square";
+    this.sideSize = sideSize;
+
+  }
+
+ }
+ 
+
+ /*ELLIPSE CLASS*/
+  
+ class Ellipse extends Shape{
+  shapeType;
+  width;
+  height;
+  
+  constructor(positionX, positionY, width, height) {
+
+      super();
+      this.positionX = positionX;
+      this.positionY = positionY;
+      this.shapeType = "ellipse";
+      this.width = width;
+      this.height = height;
+
+  }
+
+
+ }
+
+
+ /*RECTANGLE CLASS*/
+
+ class Rectangle extends Shape{
+  shapeType;
+  width;
+  height;
+
+  constructor(positionX, positionY, width, height) {
+      super();
+      this.positionX = positionX;
+      this.positionY = positionY;
+      this.width = width;
+      this.height = height;
+  }
+
+}
+
 
 class App extends Component {
 
@@ -85,109 +199,6 @@ class App extends Component {
   handleRequest(request) {
 
 
-  class Shape {
-
-    positionX;
-    positionY;
-
-    constructor(positionX, positionY) {
-      this.positionX = positionX;
-      this.positionY = positionY;
-    }
-
-    moveUp(distance){
-      this.positionY = this.positionY + distance;
-    }
-
-
-    moveDown(distance){
-      this.positionY = this.positionY - distance;
-    }
-
-
-    moveLeft(distance) {
-      this.positionX = this.positionX - distance;
-     }
- 
-    moveRight(distance ) {
-       this.positionX = this.positionX + distance;
-     }
-
-  }
-
- 
-  /*CIRCLE CLASS*/
-
-  class Circling extends Shape{
-
-    radius;
-    shapeType;
-    
-    constructor(positionX, positionY, radius) {
-      super();
-      this.positionX = positionX;
-      this.positionY = positionY;
-      this.shapeType = "circle";
-      this.radius = radius;
-    }
-  
-
-
-   }
-
-
-   /*SQUARE CLASS*/
-
-   class Square extends Shape{
-
-    shapeType;
-    sideSize;
-
-    constructor(positionx, positionY, sideSize) {
-
-      super();
-      this.shapeType = "square";
-      this.sideSize = sideSize;
-
-    }
-
-   }
-   
-
-   /*ELLIPSE CLASS*/
-    
-   class Ellipse extends Shape{
-    shapeType;
-    width;
-    height;
-    
-    constructor(positionX, positionY, width, height) {
-
-        super();
-        this.shapeType = "ellipse";
-        this.width = width;
-        this.height = height;
-
-    }
-
-
-   }
-
-
-   /*RECTANGLE CLASS*/
-
-   class Rectangle extends Shape{
-    shapeType;
-    width;
-    height;
-
-    constructor(positionX, positionY, width, height) {
-        super();
-        this.width = width;
-        this.height = height;
-    }
-
-  }
 
 
 
